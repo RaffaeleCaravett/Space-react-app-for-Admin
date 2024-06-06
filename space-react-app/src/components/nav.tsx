b510e1f39935679faaeaf54decda165b06d1a30c
RaffaeleCaravett <136345805+RaffaeleCaravett@users.noreply.github.com>
2024-06-06 21:16:28 +0200

    created a navbar

---
+++ space-react-app/src/components/nav.tsx
@@ -0,0 +1,21 @@
+const Nav = () =>{
+
+    return(
+<nav className="p-2 bg-info text-dark text-center">
+    <div className="row">
+        <div className="col-md-4 p-2">
+            Home
+        </div>
+         <div className="col-md-4 p-2">
+            Blog
+        </div>
+        <div className="col-md-4 p-2">
+       Logout
+        </div>
+    </div>
+</nav>
+
+    )
+}
+
+export default Nav
