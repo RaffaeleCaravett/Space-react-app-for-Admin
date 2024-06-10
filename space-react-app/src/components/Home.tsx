a949f1cfa0f13ccd9cfdac54d883d62c683bf3c3
RaffaeleCaravett <136345805+RaffaeleCaravett@users.noreply.github.com>
2024-06-10 17:03:08 +0200

    now doing home at its fulliest

--- space-react-app/src/components/Home.tsx
+++ space-react-app/src/components/Home.tsx
@@ -1,4 +1,6 @@
 import SingleCard from "./SingleCard";
+import image from "../assets/react.svg"
+import fwimage from "../assets/images.jpg"
 
 const Home = () => {
 
@@ -32,6 +34,21 @@ return (
 cards.content.map((card)=>(
     <SingleCard key={card.id} card={card}></SingleCard>
 )) }
+<div className="col-md-12 py-3">
+    <h1>Another awesome title</h1>
+</div>
+<div className="col-md-6 py-5">
+    <h2>Another one</h2>
+    <p className="fs-4">
+    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+    </p>
+</div>
+<div className="col-md-6 py-5 p-4">
+<img src={image} className="w-100 hover" alt="" />
+</div>
+<div className="col-md-12 py-5">
+    <img src={fwimage} alt="" className="w-100 rounded hover-shadow" />
+</div>
 </div>
         </div>
 )
