7b0411aa8e1dc489cd39ce5f39832d9a0f9ddb6c
RaffaeleCaravett <136345805+RaffaeleCaravett@users.noreply.github.com>
2024-06-11 13:07:07 +0200

    now doing form

--- space-react-app/src/components/Forms.tsx
+++ space-react-app/src/components/Forms.tsx
@@ -1,7 +1,44 @@
+
+
 const Forms = () => {
+
+ const setLogin= (bool:never)=>{
+        login=bool
+    }
+
+    const setSignup= (bool:never)=>{
+        signup=bool
+    } 
+    let login = [true,setLogin]
+    let signup = [false,setSignup]
+
+
+   
+
     return(
-<div>
-        Forms
+<div className="container text-center form-bg">
+    <div className="row">
+<div className="col-md-12 py-5">
+    <h1>Forms</h1>
+</div>
+<div className="col-md-6 py-5">
+<h2>Another one</h2>
+    <p className="fs-4">
+    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+    </p>
+</div>
+<div className="col-md-6 py-5">
+<h1>Login</h1>
+<h1>Signup</h1>
+<div className="p-2">
+{login && <form action="" className="border p-2 shadow rounded">
+
+</form>}
+{signup && <form action="" className="border p-2 shadow rounded">
+    </form>}
+</div>
+</div>
+    </div>
     </div>
     )
 }
