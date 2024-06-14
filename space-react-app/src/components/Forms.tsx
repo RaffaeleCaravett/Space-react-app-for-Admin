e60df39638eae95f5e891c72b0b4c3a1e55e82d4
RaffaeleCaravett <136345805+RaffaeleCaravett@users.noreply.github.com>
2024-06-14 21:33:08 +0200

    seems to be doing form validation properly

--- space-react-app/src/components/Forms.tsx
+++ space-react-app/src/components/Forms.tsx
@@ -12,8 +12,10 @@ const Forms = () => {
   const  [passwordError,setPasswordError] = useState('')
    
 
-const logIn = ()=>{
-  
+const logIn = (e: Event)=>{
+    e.preventDefault()
+    console.log(e)
+ 
 }
 
     return(
@@ -32,12 +34,12 @@ const logIn = ()=>{
 {login&&<h1>Login</h1>}
 {!login&&<h1>Signup</h1>}
 <div className="p-2">
-{login && <form action="" className="border p-2 shadow rounded" onSubmit={()=>logIn()}>
+{login && <form action="" className="border p-2 shadow rounded" name="" onSubmit={()=>logIn(event!)}>
     <label className="fw-bold">Email</label><br />
-    <input type="text" className="form-control shadow-lg bg-transparent" id="emailLogin" onInvalid={()=>setEmailError("Inserisci un valore tipo 'a@a.com'.")} /><br />
+    <input type="text" className="form-control shadow-lg bg-transparent" name="email" onInvalid={()=>setEmailError("Inserisci un valore tipo 'a@a.com'.")} /><br />
     {emailError!=""&&<p>{emailError}</p>}
     <label className="fw-bold">Password</label><br />
-    <input type="text" className="form-control shadow-lg bg-transparent" id="passwordLogin" minLength={6} onInvalid={()=>setPasswordError("Inserisci un valore con lunghezza minima : 6 caratteri.")}/><br />
+    <input type="text" className="form-control shadow-lg bg-transparent" name="password" minLength={6} onInvalid={()=>setPasswordError("Inserisci un valore con lunghezza minima : 6 caratteri.")}/><br />
     {passwordError!=""&&<p>{passwordError}</p>}
 <button className="btn btn-light" type="submit">Login</button>
 <hr />
