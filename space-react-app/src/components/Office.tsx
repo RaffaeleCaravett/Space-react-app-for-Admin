9c15b9a63ba72924ff73f97e98ad240a3299e569
RaffaeleCaravett <136345805+RaffaeleCaravett@users.noreply.github.com>
2024-06-16 21:44:06 +0200

    succesfuly blocked route if user is not logged in

--- space-react-app/src/components/Office.tsx
+++ space-react-app/src/components/Office.tsx
@@ -1,13 +1,22 @@
 import { useSelector } from "react-redux";
 import { tokenInterface } from "../interfaces/interfaces";
 import { useNavigate } from "react-router-dom";
+import { useEffect } from "react";
 
 const Office = () => {
  const isLoggedIn= useSelector((state:tokenInterface)=>state.accessToken.isLoggedIn)
  const navigate = useNavigate()
+ useEffect(() => {
  if(!isLoggedIn){
     navigate("/forms")
  }
+ })
+const handleWindowRefresh = () => {
+    if(window.performance.navigation.type == 1){
+        console.log('ihih')
+    }
+}
+window.addEventListener('load', handleWindowRefresh);
 
  return(
      <div>
