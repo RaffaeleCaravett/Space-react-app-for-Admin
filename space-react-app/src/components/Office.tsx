fc500ac45f6e1daa16cbdca7516793508521699d
RaffaeleCaravett <136345805+RaffaeleCaravett@users.noreply.github.com>
2024-06-18 21:41:44 +0200

    implementing auto login

--- space-react-app/src/components/Office.tsx
+++ space-react-app/src/components/Office.tsx
@@ -1,12 +1,17 @@
-import { useSelector } from "react-redux";
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import { useDispatch, useSelector } from "react-redux";
 import { tokenInterface } from "../interfaces/interfaces";
 import { useNavigate } from "react-router-dom";
 import { useEffect } from "react";
+import { setUser } from "../redux/userSlice";
 
 const Office = () => {
  const isLoggedIn= useSelector((state:tokenInterface)=>state.accessToken.isLoggedIn)
  const navigate = useNavigate()
- useEffect(() => {
+
+const api_url = useSelector((state:any) => state.api.url)
+ const dispatch = useDispatch()
+useEffect(() => {
  if(!isLoggedIn){
     navigate("/forms")
  }
@@ -14,7 +19,23 @@ const Office = () => {
 const handleWindowRefresh = () => {
     if(window.performance.navigation.type == 1){
      if(localStorage.getItem('accessToken')){
-        console.log('there is token')
+      const token = localStorage.getItem('accessToken')
+
+      fetch(`${api_url+'auth/'+token}`,{
+        method: "GET",
+        headers: {
+          "Content-Length": "0"
+        }
+    }).then((res)=>{
+        return res.json()
+    }).then((res)=>{
+        if(res){
+            dispatch(setUser(res))
+            navigate('/office')
+        }
+    }).catch((error)=>{
+        console.log(error)
+      })
      }else{
         if(localStorage.getItem('refreshToken')){
 console.log('refreshToken')
@@ -32,3 +53,5 @@ window.addEventListener('load', handleWindowRefresh);
             )
         }
 export default Office;
+
+
